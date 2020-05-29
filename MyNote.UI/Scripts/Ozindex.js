446e75c1fc1ddb78684d32ff62b69dd6506aaaae
Canberk Sahin <canberksahin20@hotmail.com>
2020-05-29 18:09:14 +0300

    new and delete note

--- MyNote.UI/Scripts/Ozindex.js
+++ MyNote.UI/Scripts/Ozindex.js
@@ -38,12 +38,7 @@ function showAppPage() {
 
             $("#notes").html("");
             for (var i = 0; i < data.length; i++) {
-                var a = $("<a/>")
-                    .attr("href","#")
-                    .addClass("list-group-item list-group-item-action show-note")
-                    .text(data[i].Title)
-                    .prop("note",data[i]);
-                $("#notes").append(a);    
+                addMenuLink(data[i]);
             }
 
             //show page when it's ready..
@@ -52,6 +47,22 @@ function showAppPage() {
         function () {
 
         });
+}
+
+function addMenuLink(note, isActive = false) {
+    var a = $("<a/>")
+        .attr("href", "#")
+        .addClass("list-group-item list-group-item-action show-note")
+        .text(note.Title)
+        .prop("note", note);
+    if (isActive) {
+        $(".show-note").removeClass("active");
+        a.addClass("active");
+        selectedLink = a.get(0);
+        selectedNote = note;
+    }
+    $("#notes").prepend(a);  
+
 }
 
 function getLoginData() {
@@ -142,13 +153,32 @@ function updateNote() {
         { Id: selectedNote.Id, Title: $("#title").val(), Content: $("#content").val() },
         function (data) {
             selectedLink.note = data;
-            selectedLink.text = data.Title;
-            alert("Güncelleme Başarılı");
+            $(selectedLink).text(data.Title);
         },
         function (data) {
 
         },
     );
+}
+
+function addNote() {
+    ajax("api/Notes/New" , "POST",
+        {Title: $("#title").val(), Content: $("#content").val() },
+        function (data) {
+            addMenuLink(data, true);
+        },
+        function () {
+
+        },
+    );
+}
+
+function resetNoteForm() {
+    selectedLink = null;
+    selectedNote = null;
+    $(".show-note").removeClass("active");
+    $("#title").val("");
+    $("#content").val("");
 }
 
 // EVENTS
@@ -222,8 +252,9 @@ $(".navbar-login a").click(function (event) {
 
 $("#btnLogout").click(function (event) {
     event.preventDefault();
-    sessionStorage.removeItem["login"];
-    localStorage.removeItem["login"];
+    resetNoteForm();
+    sessionStorage.removeItem("login");
+    localStorage.removeItem("login");
     showLoginPage();
 });
 
@@ -237,7 +268,7 @@ $("body").on("click", ".show-note", function (event) {
 
     $(".show-note").removeClass("active");
     $(this).addClass("active");
-});
+});                   
 
 $("#frmNote").submit(function (event) {
     event.preventDefault();
@@ -249,5 +280,30 @@ $("#frmNote").submit(function (event) {
     }
 });
 
+$(".add-new-note").click(function () {
+    resetNoteForm();
+});
+
+// delete note
+$("#btnDelete").click(function () {
+    if (selectedNote) {
+        if (confirm("Are you sure to delete the selected note?")) {
+            ajax("api/Notes/Delete/" + selectedNote.Id, "DELETE", null,
+                function (data) {
+                    $(selectedLink).remove();
+                    resetNoteForm();
+                },
+                function () {
+
+                }
+            );
+        }
+    }
+    else {
+        if (confirm("Are you sure to delete the draft?")) {
+            resetNoteForm();
+        }
+    }
+});
 // ACTİONS
 checkLogin();
