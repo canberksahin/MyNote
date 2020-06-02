c647f2fff72ddd78794c97f4c215ee710e1b480f
Canberk Sahin <canberksahin20@hotmail.com>
2020-06-02 21:59:46 +0300

    ui angular version, ng-view

---
+++ MyNote.UI/Scripts/index.js
@@ -0,0 +1,44 @@
+﻿// AngularJs Version
+
+var app = angular.module("myApp", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", { templateUrl: "pages/app.html" })
+        .when("/login", { templateUrl: "pages/login.html" })
+});
+
+app.controller("myCtrl", function ($scope) {
+
+    $scope.checkAuth = function () {
+        var tokenJson = localStorage["token"] | sessionStorage["token"];
+        if (!tokenJson) {
+            //display login, register view 
+
+            return;
+        }
+        // check if token is valid
+
+        //display app view
+    };
+
+    //start function
+    $scope.checkAuth();
+});
+
+// JQuery Document Ready
+$(function () {
+    $(".navbar-login a").click(function (event) {
+        event.preventDefault();
+        var href = $(this).attr("href");
+        // https://getbootstrap.com/docs/4.0/components/navs/#via-javascript
+        $('#pills-tab a[href="' + href + '"]').tab('show'); // Select tab by name
+    });
+
+    $('body').on('click', '#pills-tab a', function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    });
+});
+
+
